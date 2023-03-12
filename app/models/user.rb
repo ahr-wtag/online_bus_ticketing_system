668bcb69a3355dfe2792af859e5c6ee544c523d7
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-03-12 16:35:31 +0600

    User Model added

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    enum :role, [ :user, :admin ]
+    validates :first_name, :last_name, :email, :user_name, :encrypted_password, :phone, :role, presence: true
+    validates :email, :user_name, uniqueness: { case_sensitive: false }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :phone, numericality: true, length: {minimum: 10, maximum: 15}
+
+end
---
+++ db/migrate/20230312100332_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :email,              null: false, default: ""
+      t.string :user_name,          null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.string :phone,              null: false, default: ""
+      t.integer :role,              null: false, default: 0
+      
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                    unique: true
+    add_index :users, :user_name,                unique: true
+  end
+end
