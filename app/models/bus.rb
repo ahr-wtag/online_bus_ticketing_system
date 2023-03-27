51d407a2772f5c49ead8333250cb674548b7fd55
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-03-27 15:06:07 +0600

    added test for bus and route

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,3 +1,20 @@
 class Bus < ApplicationRecord
+    validates :name, uniqueness: { case_sensitive: false }
     validates :name, :typed, :brand, :capacity, presence: true
+
+    validates :capacity , numericality: { greater_than:0, less_than_or_equal_to: 45 }
+
+    TYPES = ['ac','non-ac']
+    validates_inclusion_of :typed, in: TYPES
+    def typed=(typed)
+        super(typed&.downcase&.strip)
+    end
+
+    def name=(name)
+        super(name&.downcase&.strip&.delete(' '))
+    end
+
+    def brand=(brand)
+        super(brand&.upcase&.strip)
+    end
 end
