77d277a154411bd9adc53daf10177192cfed16f1
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-03-27 16:36:48 +0600

    testing fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     enum role:  { user: 0, admin: 1 }
-    validates :first_name, :last_name, :email, :user_name, :encrypted_password, :phone, :role, presence: true
+    validates :email, :user_name, :encrypted_password, :phone, :role, presence: true
+    validates :first_name, :last_name, presence: true, format:{with: /\A[A-Za-z]+\z/}
     validates :email, :user_name, uniqueness: { case_sensitive: false }
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
     validates :phone, numericality: true, length: {minimum: 10, maximum: 15}
