a9e92e0ebb3f2a05915146ff4025bc3f4462ba92
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-03-28 14:27:46 +0600

    minor fix

--- spec/factories/buses.rb
+++ spec/factories/buses.rb
@@ -2,7 +2,7 @@ require 'faker'
 FactoryBot.define do
   factory :bus do
     name {Faker::Vehicle.singapore_license_plate}
-    typed {['ac','non-ac'].sample}
+    typed {['ac','non_ac'].sample}
     brand {Faker::Vehicle.manufacture}
     capacity {Faker::Number.between(from: 0, to: 45)}
   end
