4d9393abd29290c9bb76b86ae067ea51e191a79f
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-03-28 14:24:30 +0600

    test for trip and seat

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,37 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_314_095_825) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_095825) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'buses', force: :cascade do |t|
-    t.string 'name', default: '', null: false
-    t.string 'typed', default: '', null: false
-    t.string 'brand', default: '', null: false
-    t.integer 'capacity', default: 0, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "buses", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "typed", default: "", null: false
+    t.string "brand", default: "", null: false
+    t.integer "capacity", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'routes', force: :cascade do |t|
-    t.string 'origin', default: '', null: false
-    t.string 'destination', default: '', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "routes", force: :cascade do |t|
+    t.string "origin", default: "", null: false
+    t.string "destination", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'first_name', default: '', null: false
-    t.string 'last_name', default: '', null: false
-    t.string 'email', default: '', null: false
-    t.string 'user_name', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'phone', default: '', null: false
-    t.integer 'role', default: 0, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['user_name'], name: 'index_users_on_user_name', unique: true
+  create_table "seats", force: :cascade do |t|
+    t.string "number"
+    t.boolean "booked"
+    t.bigint "bus_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bus_id"], name: "index_seats_on_bus_id"
   end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer "ticket_price", default: 0, null: false
+    t.integer "total_booked", default: 0, null: false
+    t.date "date", default: "2023-03-28", null: false
+    t.time "time", default: "2000-01-01 06:40:38", null: false
+    t.bigint "bus_id", null: false
+    t.bigint "route_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bus_id"], name: "index_trips_on_bus_id"
+    t.index ["route_id"], name: "index_trips_on_route_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "user_name", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "phone", default: "", null: false
+    t.integer "role", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
+  end
+
+  add_foreign_key "seats", "buses"
+  add_foreign_key "trips", "buses"
+  add_foreign_key "trips", "routes"
 end
