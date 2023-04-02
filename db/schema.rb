45e94ec65d345e760f488ff938fe3118ff2bf23b
Md. Redwan Ahmed <redwan.ahmed@welldev.io>
2023-04-02 11:27:47 +0600

    added testing with shoulda-matcher

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_055000) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_082933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,8 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_055000) do
   create_table "trips", force: :cascade do |t|
     t.integer "ticket_price", default: 0, null: false
     t.integer "total_booked", default: 0, null: false
-    t.date "date", default: "2023-03-28", null: false
-    t.time "time", default: "2000-01-01 09:24:18", null: false
+    t.date "date", default: "2023-03-30", null: false
+    t.time "time", default: "2000-01-01 07:32:00", null: false
     t.bigint "bus_id", null: false
     t.bigint "route_id", null: false
     t.datetime "created_at", null: false
@@ -78,12 +78,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_055000) do
     t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "user_name", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "password", default: "", null: false
     t.string "phone", default: "", null: false
     t.integer "role", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,36 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  context 'when creating a ticket' do
-    let(:ticket) { FactoryBot.build(:ticket) }
-    let(:user) { FactoryBot.build(:user) }
-    let(:trip) { FactoryBot.build(:trip) }
-    let!(:bus) { FactoryBot.build(:bus) }
-    let!(:payment) { FactoryBot.build(:payment) }
 
-    it 'ensure total_fere is presence' do
-      ticket.total_fare = nil
-      expect(ticket.valid?).to eq(false)
-    end
+  let(:ticket) {FactoryBot.build(:ticket)}
+  describe 'when creating a ticket' do
+    it 'has a valid factory' do
+      expect(ticket).to be_valid
 
-    it 'ensure belongs_to user' do
-      ticket.user = user
-      expect(ticket.user != user).to eq(false)
     end
-
-    it 'ensure belongs_to trip' do
-      ticket.trip = trip
-      expect(ticket.trip != trip).to eq(false)
-    end
-
-    it 'ensure belngs_to bus' do
-      ticket.bus = bus
-      expect(ticket.bus != bus).to eq(false)
-    end
-
-    it 'ensure belngs_to payment' do
-      ticket.payment = payment
-      expect(ticket.payment != payment).to eq(false)
-    end
+    it { should validate_presence_of(:total_fare) }
+    it { should belong_to(:user) }
+    it { should belong_to(:trip) }
+    it { should belong_to(:bus) }
+    it { should belong_to(:payment) }
   end
+
 end
